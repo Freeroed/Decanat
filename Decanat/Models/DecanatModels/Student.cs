c8719ee12f6b0465d65809466fe22ec068357120
Freeroed <Telekines1551@gmail.com>
2019-11-16 01:40:29 +0300

    Добавлен абстрактный ДАО класс, можель студента

---
+++ Decanat/Models/DecanatModels/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Decanat.Models.DecanatModels
+{
+    public class Student
+    {
+        private int id { get; set; }
+        private String surname { get; set; }
+        private String firstName { get; set; }
+        private String patrinymic { get; set; }
+        private String mobileNomber { get; set; }
+        private String email { get; set; }
+        private int gruppaId { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(int id, String surname, String firstName, String patronymic) 
+        {
+            this.id = id;
+            this.surname = surname;
+            this.firstName = firstName;
+            this.patrinymic = patrinymic;
+        }
+
+
+        public override string ToString()
+        {
+            String s = surname + " " + firstName + " " + patrinymic + " " + mobileNomber + " " + email;
+            return s;
+        }
+    }
+}
