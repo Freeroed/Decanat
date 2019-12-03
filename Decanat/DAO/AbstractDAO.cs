41a77b58ec711220c373f9f7bcf146c9bbac8458
Сергей Евдокимов <telekines1551@gmail.com>
2019-12-03 20:56:02 +0300

    Добавлена строка подключенич к БД на ноутбуке

--- Decanat/DAO/AbstractDAO.cs
+++ Decanat/DAO/AbstractDAO.cs
@@ -9,7 +9,9 @@ namespace Decanat.DAO
 {
     public class AbstractDAO
     {
-        private const string connectionString = "Data Source=DESKTOP-4NGH6J2; Initial Catalog=model;Integrated Security=True";
+        //private const string connectionString = "Data Source=DESKTOP-4NGH6J2; Initial Catalog=model;Integrated Security=True"; // Для компа
+        private const string connectionString = "Data Source=DESKTOP-FQ15R95\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True"; // Для ноута
+
         public static Logger loger = LogManager.GetCurrentClassLogger();
 
         protected SqlConnection Connection { get; set; }
